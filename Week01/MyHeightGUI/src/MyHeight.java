58d47c76fe6e940223d0d74c8db0181e3334deda
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-02 17:37:16 +0100

    not sure if this will work

--- Week01/MyHeightGUI/src/MyHeight.java
+++ Week01/MyHeightGUI/src/MyHeight.java
@@ -1,3 +1,7 @@
+import java.lang;
+import javax.swing;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
