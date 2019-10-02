02a7dd4b1f810454ffae3b89e4cf5c366e0b8998
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-02 17:35:45 +0100

    Add files via upload

---
+++ BankExample1/src/bankentities/BankAccount.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankentities;
+
+/**
+ *
+ * @author Student
+ */
+public class BankAccount {
+    private int balance;
+    private int overdraft;
+    private String holder;
+    
+    public BankAccount(String holder){
+     this.holder = holder;
+     this.overdraft = 500;
+     this.balance = 100;
+    }
+    
+    public void depositMoney(int amount){
+        balance += amount;
+        
+    }
+    
+    public boolean withdrawMoney(int amount){
+        
+        if((balance + overdraft) < amount){
+            return false;
+        }
+        else{
+            balance -= amount;
+            return true;
+        }
+    }
+    
+    public int getBalance(){
+        return balance;
+    }
+    public String getHolder(){
+        return holder;
+    }
+    
+    public int getOverdraft(){
+        return overdraft;
+    }
+    
+    public void setOverdraft(int overdraft){
+        this.overdraft = overdraft;
+    }
+    
+    
+}
